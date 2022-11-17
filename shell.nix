63ffe397b1cbd3dacfbe127ec793f0bf79e8615c
Chris Martin <ch.martin@gmail.com>
2022-11-17 11:50:40 -0700

    improve nix shell

--- shell.nix
+++ shell.nix
@@ -5,6 +5,8 @@ inherit ((pkgs.callPackage ./. {}).haskellPackages) chris-martin-org;
 
 in pkgs.mkShell {
     name = "chris-martin-org-shell";
-    inputsFrom = [ chris-martin-org ];
-    packages = [ pkgs.sassc ];
+    inputsFrom = [ chris-martin-org.env ];
+    packages = [ pkgs.sassc pkgs.cabal-install pkgs.zlib ];
+    LC_ALL = "en_US.UTF-8";
+    LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
 }
