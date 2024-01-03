6a54be42fdeec4534f5f7d38387f73b338d7b86b
Chris Martin <ch.martin@gmail.com>
2024-01-03 00:41:50 -0700

    DO deploy

--- Makefile
+++ Makefile
@@ -1,7 +1,4 @@
 build:
 	cabal run site
 
-deploy:
-	rsync -avz -e 'ssh' out/ chris-martin.org:/var/www/chris-martin.org/
-
-.PHONY: build deploy
+.PHONY: build
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    "nixos-23.11".url = "github:NixOS/nixpkgs/nixos-23.11";
+    "nixos-unstable".url = "github:NixOS/nixpkgs/nixos-unstable";
+    "flake-utils".url = "github:numtide/flake-utils";
+  };
+  outputs = inputs@{ self, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        nixpkgs = {
+          "nixos-23.11" = import inputs."nixos-23.11" { inherit system; };
+          "nixos-unstable" = import inputs."nixos-unstable" { inherit system; };
+        };
+        pkgs = nixpkgs."nixos-23.11";
+        inherit (pkgs) runCommand crane;
+        inherit (pkgs.lib) fold composeExtensions concatMap attrValues;
+        inherit (pkgs.haskell.lib) justStaticExecutables;
+
+        combineOverrides = old:
+          fold composeExtensions (old.overrides or (_: _: { }));
+
+        haskellPackages = pkgs.haskellPackages.override {
+          overrides = new: old: {
+            chris-martin-org = new.callPackage ./chris-martin-org.nix {};
+          };
+        };
+
+        htmlPages = ./out;
+
+        container = pkgs.dockerTools.buildLayeredImage {
+          name = "chris-martin-org";
+          contents = [ pkgs.busybox htmlPages ];
+          config = {
+            Cmd = [ "${pkgs.busybox}/bin/busybox" "httpd" "-f" "-h" "${htmlPages}" ];
+            ExposedPorts."80/tcp" = { };
+          };
+        };
+
+        deploy = pkgs.writeShellApplication {
+          name = "deploy-chris-martin";
+          runtimeInputs = [ crane ];
+          text = ''
+            cleanup() {
+              rm -rf "$tmp"
+            }
+            trap cleanup EXIT
+            tmp="$(mktemp -d)"
+            nix build .#container --out-link "$tmp/container.tar.gz"
+            gunzip --force "$tmp/container.tar.gz" > "$tmp/container.tar"
+            crane push "$tmp/container.tar" registry.digitalocean.com/iowa/chris-martin:latest
+          '';
+        };
+
+      in {
+        packages = { inherit container htmlPages crane; };
+        devShells.deploy = pkgs.mkShell { packages = [ deploy ]; };
+      }
+    );
+}
